f6f63eef27e2bc5ab7a27535eabf9cca1c261c7e
Eric Eslinger <eric.eslinger@gmail.com>
2017-02-15 19:22:00 -0800

    tests passing but bug exposed

--- src/test/children.spec.js
+++ src/test/children.spec.js
@@ -32,9 +32,62 @@ describe('HasMany Plump Routes', () => {
       });
     })
     .then((response) => {
-      console.log(response.payload);
       expect(response).to.have.property('statusCode', 200);
-      return expect(one.$get('children')).to.eventually.deep.equal({ children: [100] });
+      return expect(one.$get('children')).to.eventually.deep.equal({ children: [{ id: 100 }] });
+    });
+  });
+
+  it('R', () => {
+    const one = new TestType({ name: 'potato' }, plump);
+    return one.$save()
+    .then(() => one.$add('children', 100))
+    .then(() => {
+      return hapi.inject({
+        method: 'GET',
+        url: `/api/${one.$id}/children`,
+      });
+    })
+    .then((response) => {
+      expect(response).to.have.property('statusCode', 200);
+      return expect(one.$get('children')).to.eventually.deep.equal({ children: [{ id: 100 }] });
+    });
+  });
+
+  it('U', () => {
+    const one = new TestType({ name: 'potato' }, plump);
+    return one.$save()
+    .then(() => one.$add('valenceChildren', 100, { perm: 2 }))
+    .then(() => {
+      return expect(one.$get('valenceChildren')).to.eventually.deep.equal({ valenceChildren: [{ id: 100, perm: 2 }] });
+    })
+    .then(() => {
+      return hapi.inject({
+        method: 'PATCH',
+        url: `/api/${one.$id}/valenceChildren/100`,
+        payload: JSON.stringify({ perm: 3 }),
+      });
+    })
+    .then(() => {
+      return expect(plump.find('tests', one.$id).$get('valenceChildren'))
+        .to.eventually.deep.equal({ valenceChildren: [{ id: 100, perm: 3 }] });
+    });
+  });
+
+  it('D', () => {
+    const one = new TestType({ name: 'potato' }, plump);
+    return one.$save()
+    .then(() => one.$add('children', 100))
+    .then(() => {
+      return expect(one.$get('children')).to.eventually.deep.equal({ children: [{ id: 100 }] });
+    })
+    .then(() => {
+      return hapi.inject({
+        method: 'DELETE',
+        url: `/api/${one.$id}/children/100`,
+      });
+    })
+    .then(() => {
+      return expect(plump.find('tests', one.$id).$get('children')).to.eventually.deep.equal({ children: [] });
     });
   });
 });
